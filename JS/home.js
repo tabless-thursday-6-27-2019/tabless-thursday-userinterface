6234e0c71407bd64e092e6e910e926cb3fdd1d92
Matt Herich <mjherich@gmail.com>
2019-06-26 15:38:59 -0700

    Add parallax to big t

---
+++ JS/home.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+
+
+    // Big T parallax effect on scroll
+    // Get references for big T's
+    const firstBigT = document.querySelector('#what-is-it .big-t-el')
+    const secondBigT = document.querySelector('#pricing .big-t-el')
+    // Initialize multiplier var
+    let multiplier = 0
+    // Add scroll event listener
+    this.addEventListener('scroll', e => {
+        // console.log(this.scrollY)
+        if (window.scrollY < 1000) {
+            multiplier = 0.1 * this.scrollY
+            firstBigT.setAttribute('style', `transform: translateY(${multiplier}px)`)
+        } else if (window.scrollY > 2800) {
+            multiplier = 0.1 * (this.scrollY - 2800)
+            secondBigT.setAttribute('style', `transform: translateY(${multiplier}px)`)
+        }
+    })
+
+
+})
