b89aaa2eb1e05f96228f431063f00807cd955e19
Daniel <danielkim.s@outlook.com>
2019-06-28 08:06:38 -0700

    Added event listener

--- JS/about.js
+++ JS/about.js
@@ -1,6 +1,6 @@
 const profile = document.querySelectorAll('.member');
 profile.forEach(profile =>{profile.addEventListener('mouseenter', function(event){
     console.log(".member");
-  event.target.style.border = '4px solid #E4BE4D';
+  event.target.style.border = '8px solid grey';
 })})
     
