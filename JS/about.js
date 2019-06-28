5d4afbf6f0f1c61b413b45bce4d5f6e893e272a3
Daniel <danielkim.s@outlook.com>
2019-06-27 17:09:26 -0700

    Changed NL color to yellow, made progress on EventListener

--- JS/about.js
+++ JS/about.js
@@ -1,4 +1,6 @@
-const profile = document.querySelector('#member1');
-profile.addEventListener('mouseover', function(event){
+const profile = document.querySelectorAll('.member');
+profile.forEach(profile =>{profile.addEventListener('mouseenter', function(event){
+    console.log(".member");
   event.target.style.border = '4px solid #E4BE4D';
-})
+})})
+    
